f0e6f4a0e70ab73288dbafa3917f06c2aec0aa55
Andreas Lindberg <oaflindberg@yahoo.com>
2022-09-05 16:07:42 +0200

    Test

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<!-- <script>
+<script>
   import { intervalToDuration } from 'date-fns';
   import Bat from './lib/Bat.svelte';
   import Confetti from './lib/Confetti.svelte';
@@ -23,7 +23,7 @@
   };
 
   setInterval(getDiff, 200);
-</script> -->
+</script>
 
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
